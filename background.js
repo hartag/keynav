6b88518f8c4a873db8ba6266fb7be052c633cb90
ahart <ahart@dim.uchile.cl>
2023-08-10 17:15:38 -0400

    changed What's New message.
    . Updated The What's New message to report the changes made in V4.1.
    
    . Made it so the What's New message only appears on upgrade now.
    
    . Added a little more help to the extension description message.
    
    . Removed the Help button contemplated in a recent commit.

--- background.js
+++ background.js
@@ -15,7 +15,7 @@ messenger.commands.onCommand.addListener((command, tab) => {
 	
 // Show a What's New dialog
 messenger.runtime.onInstalled.addListener(	details => {
-  //if (details.reason=="update") {
+  if (details.reason=="update") {
     messenger.windows.create({
       allowScriptsToClose: true,
       //focused: true,
@@ -23,5 +23,5 @@ messenger.runtime.onInstalled.addListener(	details => {
       type: "popup",
       url: "whatsnew/whatsnew.html"
     });
-  //}
+  }
 });
